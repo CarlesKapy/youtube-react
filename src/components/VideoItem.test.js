83e9a61dbe8f01790e8e65be3a3209cb9d70669e
Carles Capell <carles.capell.87@gmail.com>
2017-10-25 19:35:33 +0200

    Another test

--- src/components/VideoItem.test.js
+++ src/components/VideoItem.test.js
@@ -4,14 +4,23 @@ import VideoItem from './VideoItem'
 import type { Video } from './types'
 
 describe ('VideoItem', () => {
+
+    const video = {
+        "id": "4TPqUvy1vYU", 
+        "thumbnail": "https://i.ytimg.com/vi/4TPqUvy1vYU/mqdefault.jpg",
+        "title": "St. Vincent - New York (Official Video)"
+    };
+
     it('renders correctly', () => {
-        const video = {
-            "id": "4TPqUvy1vYU", 
-            "thumbnail": "https://i.ytimg.com/vi/4TPqUvy1vYU/mqdefault.jpg",
-            "title": "St. Vincent - New York (Official Video)"
-        };
+        
         const component = shallow(<VideoItem video = {video} />)
         expect(component).toMatchSnapshot();
+    })
+
+    it('render image correctly', () => {
+        const component = shallow(<VideoItem video={video}/>)
+		const image = <img src={video.thumbnail} alt={video.title} />
+		expect(component.contains(image)).toBeTruthy()
     })
     
 })
